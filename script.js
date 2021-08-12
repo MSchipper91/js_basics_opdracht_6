37606b1a238f00af6a402e36686a4a1b6f4f2fdd
Melanie Schipper <78080525+MSchipper91@users.noreply.github.com>
2021-08-12 15:10:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const makingPizza = function() {
+    console.log ('cut vegetables')
+    console.log ('make dough')
+    console.log ('roll dough')
+    console.log ('put tomato sauce on dough')
+    console.log ('put vegetables on dough')
+    console.log ('put meat on dough')
+    console.log ('put pizza in oven')
+}
+
+const makingSushi = function() {
+    console.log ('boil rice')
+    console.log ('cut fish')
+    console.log ('cut vegetables')
+    console.log ('roll everything in seaweed')
+    console.log ('add soy sauce')
+}
+
+makingSushi();
+makingSushi();
+makingSushi();
+makingPizza();
+makingPizza();
